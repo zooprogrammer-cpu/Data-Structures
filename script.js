90842b0b02b92cddea8bf6513e846ed06aff41cd
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-08-28 15:07:59 -0500

    destructuring

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order : function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +30,35 @@ const restaurant = {
     },
   },
 };
+
+// const arr = [2,3,4]
+// const [x,y,z] = arr
+// console.log(x,y,z);
+
+let [main, secondary] = restaurant.categories
+console.log(main,secondary); // Italian – "Pizzeria"
+// Flip variables
+[main,secondary] = [secondary, main] 
+
+console.log(main,secondary); //Pizzeria – "Italian"
+
+console.log(restaurant.order(2,0)) //["Garlic Bread", "Pizza"] 
+
+//Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2,0) 
+console.log([starter, mainCourse]); //prints ["Garlic Bread", "Pizza"]
+
+//Nested destructuring
+const nested = [2,4,[5,6]]
+
+const [i,,[j,k]] = nested
+
+console.log(i,j,k); // prints 2,5,6
+
+//Default values
+// When we do not know the length of an array
+const [p,q,r] = [8,9]
+console.log(p,q,r); // undefined for r since we did not know it was empty
+// so we can setup default values
+const [m=1,n=1,o=1] = [8,9]
+console.log(m,n,o); //prints 8,9,1 sets the value of o as 1
