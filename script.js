e4a81025a0e5a8cdfdf3cf943b2384c3d08cb01c
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-08-30 14:33:14 -0500

    Coding Challenge 1 Spread, Rest and Logical

--- script.js
+++ script.js
@@ -1,103 +1,82 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
-// Data needed for first part of the section
-// const restaurant = {
-//   restName: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-//   order : function(starterIndex, mainIndex){
-//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
-//   },
-
-//   orderDelivery: function ({starterIndex, mainIndex, time, address}){
-//     console.log(`Order Received ${starterIndex} and ${mainIndex} and ${this.mainMenu[mainIndex]}
-//     will be delivered to ${address} at ${time} `)
-//   },
-
-//   orderPasta : function(ing1, ing2, ing3){
-//     console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
-//   },
-
-//   orderPizza : function(mainIngredient, ...otherIngredients){
-//     console.log(mainIngredient);
-//     console.log(otherIngredients);
-//   },
-
-//   openingHours: {
-//     thu: {
-//       open: 12,
-//       close: 22,
-//     },
-//     fri: {
-//       open: 11,
-//       close: 23,
-//     },
-//     sat: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
-//   },
-
-// }
-// //Nullish coalescing operator ??
-// restaurant.numGuests = 0; 
-// const guests = restaurant.numGuests || 10;
-// console.log(guests); // Prints 10 since 0 is a falsy value. But we were expecting 0. 
-
-// //New is ES 2020, ?? same as || but produces the correct result.
-// // it works on the idea of nullish values instead of falsy values
-// // nullish values are null and undefined (NOT 0 or ''). treats 0 or '' as true
-// const correctGuests = restaurant.numGuests ?? 10;
-// console.log(correctGuests); // prints 0
-
-const rest1 = {
-  name : 'Capri', 
-  // numGuests : 20
-  numGuests : 0
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
 };
+// 1. Create one player array for each team (variables 'players1' and 'players2')
+//destructure game.players into players1 and players2
+const [players1, players2] = game.players
 
-const rest2 ={
-  name: 'La Piazza',
-  owner : "Giovanni Rossi"
+console.log(players1);
+console.log(players2);
+// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+
+const [gk,...fieldPlayers] = players1
+//3. Create an array 'allPlayers' containing all players of both teams (22 players)
+
+const allPlayers = [...players1, ...players2]
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. 
+//So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+const players1Final = [...players1,'Thiago','Coutinho','Perisic']
+
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+const {odds:{team1, x:draw, team2}} = game
+console.log(team1,draw,team2);
+
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) 
+// and prints each of them to the console, along with the number of goals that were scored 
+// in total (number of player names passed in)
+
+const printGoal = function (...playerNames){
+  console.log(`${playerNames.length} goals were scored`);
 }
-// OR assignment operator 
-// rest1.numGuests = rest1.numGuests || 10
-// rest2.numGuests = rest2.numGuests || 10
 
-// console.log(rest1);
-// console.log(rest2);
+printGoal('Davies', 'Muller', 'Lewandowski','Kimmich')
+printGoal('Davies', 'Muller')
+printGoal(...game.scored)
 
-// Logical assignement operator, same as above but new
-// rest1.numGuests ||= 10
-// rest2.numGuests ||= 10
+//7. The team with the lower odd is more likely to win.
+// Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+// team with lower odd is more likely to win 
 
-// console.log(rest1); // prints {name: "Capri", numGuests: 20}
-// console.log(rest2); // prints {name: "La Piazza", owner: "Giovanni Rossi", numGuests: 10}
-
-//If numGuests : 0, we run into an issue since 0 is falsy and prints 10. 
-//Solution is to use nullish coalescing logical assignment operator (null or undefined)
-// rest1.numGuests ??= 10
-// rest2.numGuests ??= 10
-
-// console.log(rest1); //prints {name: "Capri", numGuests: 0}
-// console.log(rest2); //prints  {name: "La Piazza", owner: "Giovanni Rossi", numGuests: 10}
-
-//AND assignment operator
-// rest1.owner = rest1.owner && '<ANONYMOUS>';
-// rest2.owner = rest2.owner && '<ANONYMOUS>';
-
-//console.log(rest1); //prints {name: "Capri", numGuests: 0, owner: undefined}
-//console.log(rest2); //prints {name: "La Piazza", owner: "<ANONYMOUS>"}
-
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
-
-console.log(rest1); //prints [Log] {name: "Capri", numGuests: 0} 
-console.log(rest2); //prints {name: "La Piazza", owner: "<ANONYMOUS>"}
-
+team1 < team2 &&  
+console.log(`Team 1 is more likley to win`);
+team1 > team2 &&  
+console.log(`Team 2 is more likley to win`);
