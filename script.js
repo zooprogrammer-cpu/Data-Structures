6bca0dd5d7e880cd042bfeece0b68c41d971f96d
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-03-07 22:31:49 -0600

    call and apply

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ const capitalizeName = function (name){
 
 capitalizeName('jessica ann arbor')
 
+
 //another way
 // let arrName=[]
 // const capitalizeName = function (name){
@@ -106,3 +107,63 @@ console.log(maskCreditCard(13213123435356)) //**********5356
 const message2 = 'Bad weather..All Departures Delayed'
 console.log(message2.repeat(4));
 
+// optional chaining
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // ES6 enhanced object literals
+  openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+//optional chaining
+
+for (const day of weekdays){
+  // console.log(item);
+	// const open = restaurant.openingHours[day]?.open || 'closed'; //prints closed as default
+
+  //using nullish coalescing ?? here instead of || since sat.open is 0 and we don't want to print closed
+  
+	const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}day, we open at ${open} `);
+}
